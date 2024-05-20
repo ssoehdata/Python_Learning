72a833e796780b07a4ea00fafa8412bd9936cda7
ssoehdata <150803481+ssoehdata@users.noreply.github.com>
2024-05-20 20:18:35 +0200

    Update readme.py

--- PyMLBasics/readme.py
+++ PyMLBasics/readme.py
@@ -1,3 +1,3 @@
  # https://www.geeksforgeeks.org/matrix-manipulation-python/
 
- # covers a lot of operations and examples of python ML operations etc
+ # working through examples of python ML operations etc
